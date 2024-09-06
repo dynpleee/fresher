a93cd8b22616bd7ad577f81152951a23b3e71903
duyen_nguyen <Anhaokieu291296@gmail.com>
2024-09-07 01:17:53 +0700

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,22 +1,44 @@
 package com.VMOproject.fresher.controller;
 
 import com.VMOproject.fresher.dto.request.UserCreationRequest;
+import com.VMOproject.fresher.dto.request.UserUpdateRequest;
 import com.VMOproject.fresher.entity.User;
-import com.VMOproject.fresher.repository.UserRepository;
 import com.VMOproject.fresher.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @PostMapping
     User createUser(@RequestBody UserCreationRequest request){
-        return userService.createUser((UserRepository) request);
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId){
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request){
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId){
+        userService.deleteUser(userId);
+        return "User has been deleted";
     }
 }
 
