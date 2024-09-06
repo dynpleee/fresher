538d2cc68b44f29caca1494ed3495614817f5c55
duyen_nguyen <Anhaokieu291296@gmail.com>
2024-09-07 01:16:34 +0700

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -1,9 +1,12 @@
 package com.VMOproject.fresher.service;
 
+import com.VMOproject.fresher.dto.request.UserUpdateRequest;
 import com.VMOproject.fresher.entity.User;
 import com.VMOproject.fresher.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -22,4 +25,19 @@ public class UserService {
         return userRepository.save(user);
 
     }
+    public User getUser(String userId) {
+        return null;
+    }
+
+    public List<User> getUsers() {
+        return null;
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        return null;
+    }
+
+    public void deleteUser(String userId) {
+    }
+
 }
